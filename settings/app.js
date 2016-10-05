70bafa77e9927a6162c5357fba9dfb2ef60a3441
Victor Vranceanu <vranceanu.victor@gmail.com>
2016-10-05 17:01:32 +0300

    Added public static folder and logos for supported providers

--- settings/app.js
+++ settings/app.js
@@ -1,10 +1,13 @@
 
 // app config
 module.exports = function(app){
+	var express      = require('express');
 	var mongoose     = require('mongoose');
 	var bodyParser   = require('body-parser');
 	var passport     = require('passport');
 	var set          = require('../settings').set;
+
+	app.use(express.static('public'));
 
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
